1a0277441fb078b8c372836117e1e76ccff0308a
Gian Lucas <lgian584@gmail.com>
2022-11-03 10:48:51 -0300

    app user team router

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,15 @@ const port = 3333 || process.env.PORT;
 const cors = require("cors");
 
 const userRouter = require("./routes/user");
+const teamRouter = require("./routes/team");
+
+// https://flagicons.lipis.dev/flags/4x3/ar.svg
 
 app.use(json());
 app.use(cors());
 
 app.use("/users", userRouter);
+app.use("/teams", teamRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
